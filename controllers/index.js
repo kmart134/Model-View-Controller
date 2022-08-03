05d3207da93e09e8a0d8ea448650b83d33de63e9
kmart134 <ksmgps@hotmail.com>
2022-08-03 13:41:11 -0400

    updated controllers api index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 //from activity
-const otherRoutes = require('./routes');
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashBoard = require('./dashboardRoutes');
 
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/', dashBoard);
+//^does this look right?
 
 module.exports = router;
