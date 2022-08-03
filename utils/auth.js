cf115c187863595269e0ddf4210a683a55b6bd2f
kmart134 <ksmgps@hotmail.com>
2022-08-03 13:14:41 -0400

    auth.js updated

--- utils/auth.js
+++ utils/auth.js
@@ -2,6 +2,14 @@ const withAuth = (req, res, next) => {
     //conditional that checks if the user is logged in
     //if not- send to login page
     //else- next();
-}
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+      } else {
+        next();
+      }
+    };
+    
+    module.exports = withAuth;
+
 
 //export withAuth;
