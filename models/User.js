ce043363838bbb92e1dc750bcf4f0e635f3a3203
kmart134 <ksmgps@hotmail.com>
2022-08-03 20:12:49 -0400

    updated User.js model

--- models/User.js
+++ models/User.js
@@ -1,15 +1,49 @@
 //user extends model
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
 //method to run on every instance to check password - using bcrypt
 
+class User extends Model {}
 
+//hooks works with bcrypt???
 
+User.init(
 //User.init
 //id, data type int, allow null false Primary Key
 //user_name  type string, allow nul false
 //password type string, allow nul false, validate
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [2,10]  
+          }
+      }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
 
-//hooks works with bcrypt
+
+
+module.exports = User;
+
+
+
  
-//ALL MODELS of these should include sequelize as part of the init
-
-// module.export= comment
