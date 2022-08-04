e0317f6d5206612369337332e08c9f36ea3277c0
kmart134 <ksmgps@hotmail.com>
2022-08-04 15:48:08 -0400

    commentRoutes and dashBoard routes updated

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,24 @@
 //router
+// const { comment } = require("./api");
+const withAuth = require("../utils/auth");
+const { Comment } = require("../models");
+const router = require('express').Router();
 //Comment from models
 //withAuth
 
 //post that creates a comment
+
+router.post('/', withAuth, async (req,res) => {
+    try{
+        const newComment = await Comment.create({
+            ...req.body,
+            userId: req.session.userId,
+        });
+        res.json(newComment);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,20 +2,43 @@
 
 const { post } = require("./api");
 const withAuth = require("../utils/auth");
+const { Post } = require("../models");
 const router = require('express').Router();
 
 //get route that uses withAuth - uses finadAll on post, where user id
 //render all-posts-admin with dashboard layout
 
+router.get('/', withAuth, async (req,res) => {
+    try{
+        const postData = await Post.findAll({
+            where:{
+                userId: req.session.userId,
+            },
+        })
+
+        const posts = postData.map((posts) => post.get({plain: true}));
+        res.render('all-posts-admin', {
+            layout: 'dashboard', 
+            posts,})
+    } catch(err){res.redirect('login'); 
+}
+})
+
 //get route for rendering the new post page, also using withAuth -findAll
 //render new-posts, with dashboard layout
+
+router.get('/new', withAuth, async (req,res) =>{
+    res.render('new-post',{
+        layout: 'dashboard',
+    });
+})
 
 
 
 //get route for editing a post, using withAuth - findbyPK
 router.get('edit/:id', withAuth, async (req, res) => {
     try{
-        const postData = await post.findByPK(req.params.id);
+        const postData = await Post.findByPK(req.params.id);
         if(postData){
             const post = postData.get({plain:true});
             res.render('edit-post', {
@@ -30,4 +53,6 @@ router.get('edit/:id', withAuth, async (req, res) => {
         res.redirect('login')
         //through error with redirect to login page
     }
-})
+});
+
+module.exports = router;
