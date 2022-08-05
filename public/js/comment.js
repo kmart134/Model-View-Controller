d47249523440b15280869eef508740d4da60c6cd
kmart134 <ksmgps@hotmail.com>
2022-08-04 20:25:17 -0400

    comment.js in public/js file updated

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,2 +1,19 @@
-//SELECT sertain elements from page using query selectores 
+//SELECT certain elements from page using query selectores 
 //then, grab those values and use a fetch to send them over
+
+const { json } = require("sequelize/types")
+
+const commentHandler = async function(event){
+    event.preventDefault()
+    const postId = document.querySelector('input[name = "post-id"]').value
+    const body = document.querySelector('textarea[name="comment-body"]').value 
+
+    if(body){
+        await fetch('/api/comment', {
+            method:'POST',
+            body: JSON.stringify({
+                postId, body
+            })
+        })
+    }
+}
